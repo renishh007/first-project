c2d9714fc60c005ea7e88a2bec623284b3f90040
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-23 16:27:18 +0530

    Remove mistaken JavaScript submodule

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 51236
+let accoountEmail = "renish@gmail.com"
+var accountPassword = "852369"
+accountCity = "Ahmedabad"
+
+
+console.log(accountId);
+
+
+accountEmail = "renish01@gmail.com"
+accountPassword = "254045"
+
+console.log(accountEmail);
+console.table([accountId, accountPassword])
